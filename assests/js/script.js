635958721f091d8bcd0f79e94b0b614ef2c26db2
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-10 13:36:37 -0800

    added ability to add task

---
+++ assests/js/script.js
@@ -0,0 +1,21 @@
+const buttonEl = document.querySelector("#save-task");
+const tasksToDoEl = document.querySelector("#tasks-to-do");
+
+const createTaskHandler = function() {
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+
+//////// Steps to add to DOM using JS///////////
+// 1. create new element
+// 2. style the element
+// 3. add text to the element
+// 4. append element to the DOM
+
+//Questions 
+//When to use () after function name and when not to
