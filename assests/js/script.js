58d23e19742dd67b1d656a90735843114c591353
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-11 21:31:43 -0800

    Added functionality to add task and task type

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,14 +2,30 @@ const buttonEl = document.querySelector("#save-task");
 const tasksToDoEl = document.querySelector("#tasks-to-do");
 const formEl = document.querySelector("#task-form");
 
+
 const createTaskHandler = function(event) {
     event.preventDefault();
-    
-    let listItemEl = document.createElement("li");
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
+    const listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list item
+    const taskInfoEl = document.createElement("div");
+    // Give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-}
+    console.dir(listItemEl);
+
+} 
 
 formEl.addEventListener("submit", createTaskHandler);
 
@@ -20,5 +36,5 @@ formEl.addEventListener("submit", createTaskHandler);
 // 3. add text to the element
 // 4. append element to the DOM
 
-//Questions 
-//When to use () after function name and when not to
+
+// "We we right tags in our HTML  in order to create elements in the DOM"
